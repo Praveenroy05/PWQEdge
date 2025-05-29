4e1abecca2394928680db75901bf9db27bebec72
Praveen <praveen.k@xnara.com>
2025-05-29 20:02:56 +0530

    added dropdown

---
+++ tests/dropDown.spec.ts
@@ -0,0 +1,27 @@
+// Drop down
+
+// 2 Types of drop down
+// 1. Static drop down - The value might never get changed
+    // a. Single select
+    // b. Multi select
+// 2. Dynamic drop down - The value might get changed based on some conditions
+     // a. Single select
+     // b. Multi select
+
+
+
+// select tag - <select> </select>
+
+// non-select tag - div, span, li, ul
+
+// Algorithm to handle a DD that have been developed using <select>
+
+// 1. locate the drop down element
+// 2. select the element or value from drop down using selectOption("value" or label or index)
+
+// Algorithm to handle a DD that have been developed using non-select tag
+
+// 1. locate the drop down element
+// 2. click on the drop down element
+// 3. locate the target element from the 
+// 4. click on the target element
--- tests/mouseOperations.spec.ts
+++ tests/mouseOperations.spec.ts
@@ -17,3 +17,34 @@ test("Mouse Operations", async ({page})=>{
     await page.getByText("right click me").click({button : 'right'})
     await page.getByText("Edit").click()
 })
+
+test("Mouse hover", async ({page})=>{
+    await page.goto("https://www.spicejet.com/")
+
+    // hover() - Mouse over on an element
+    await page.getByText("Add-ons", {exact : true}).hover()
+    await expect(page.getByTestId("test-id-Extra Seat")).toBeVisible()
+})
+
+test("Drag and drop", async ({page})=>{
+    await page.goto("https://jqueryui.com/resources/demos/droppable/default.html")
+    // source element - draggable element
+    // target element - droppable element
+    const source = await page.locator("div#draggable")
+    const target = page.locator("div#droppable")
+
+    //dragTo(Locator) - Drag the source element towards the target element and drop it.
+    await source.dragTo(target)
+})
+
+test("Scroll down", async ({page})=>{
+    await page.goto("https://playwright.dev/docs/locators")
+
+    // await page.locator(".pagination-nav__label").last().click()
+    // await page.locator("[href='#locate-by-role']").first().click()
+
+    await page.locator(".pagination-nav__label").last().scrollIntoViewIfNeeded()
+    await page.locator(".pagination-nav__label").last().click()
+
+})
+

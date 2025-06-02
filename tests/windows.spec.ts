316c178c94106e333b5579d529f147bef5e78b62
Praveen <praveen.k@xnara.com>
2025-06-02 19:31:35 +0530

    added alerts

---
+++ tests/windows.spec.ts
@@ -0,0 +1,9 @@
+// windows - popup events appeared on the page
+
+// Launch the url - page.goto()
+// waitForEvent('popup') - page.waitForEvent() - without using await 
+// click on the element - whichever is responsible for creating a new page/tab/window
+// const newPage = await successfull message - it will return us the new page handle - newPage
+// validate or click something on new page - newPage.click()
+// ..........
+// page.locator().click()
